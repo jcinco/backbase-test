661940de575e717949d35854e1b37dc9a8f48d26
jcinco <john.cinco@gmail.com>
2019-05-03 07:04:44 +0800

    Added models for the json data

--- BackBaseExam/BackBaseExam/Model/CitiesModel.swift
+++ BackBaseExam/BackBaseExam/Model/CitiesModel.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+public class CitiesModel:NSObject {
+    
+    
+    private func loadJson() {
+        do {
+            let jsonFilePath = Bundle.main.path(forResource: "cities", ofType: "json")
+            let data = try Data(contentsOf: URL(fileURLWithPath: jsonFilePath!))
+            let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! [[String:Any?]]
+            
+            // parse json
+            self.parseJson(json: json)
+        }
+        catch let e as NSError {
+            print(e.localizedDescription)
+        }
+    }
+    
+    private func parseJson(json:Array<[String:Any?]>?) {
+        guard let inJson = json else {
+            print("Nil json array")
+            return
+        }
+        
+        for item in inJson {
+            
+        }
+        
+    }
+    
+    
+    // MARK: - Singleton implementation
+    private static var INSTANCE:CitiesModel!
+    private override init() {
+        super.init()
+        self.loadJson()
+    }
+    public static var sharedInstance:CitiesModel! {
+        get {
+            if (nil == INSTANCE) {
+                INSTANCE = CitiesModel()
+            }
+            return INSTANCE
+        }
+    }
+}
--- BackBaseExam/BackBaseExam/Model/City.swift
+++ BackBaseExam/BackBaseExam/Model/City.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+/*
+ {
+ "country":"UA",
+ "name":"Hurzuf",
+ "_id":707860,
+ "coord":{
+ "lon":34.283333,
+ "lat":44.549999
+ }
+ }
+ */
+public struct City:Codable {
+    public var country:String?
+    public var name:String?
+    public var id:Int?
+    public var coord:Coordinates?
+}
+
+public struct Coordinates:Codable {
+    public var lon:Float?
+    public var lat:Float?
+}
