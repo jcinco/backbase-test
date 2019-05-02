c2cb72d4f0e0d464b34f1cbe9b65bcbc489562eb
jcinco <john.cinco@gmail.com>
2019-05-03 06:45:04 +0800

    Initial commit - work on going

---
+++ BackBaseExam/BackBaseExam/Model/City.swift
@@ -0,0 +1,9 @@
+//
+//  City.swift
+//  BackBaseExam
+//
+//  Created by John Freidrich Cinco on 03/05/2019.
+//  Copyright © 2019 John Freidrich Cinco. All rights reserved.
+//
+
+import Foundation
---
+++ BackBaseExam/BackBaseExam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BackBaseExam
+//
+//  Created by John Freidrich Cinco on 03/05/2019.
+//  Copyright © 2019 John Freidrich Cinco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
